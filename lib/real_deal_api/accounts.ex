518a222f1b227bb68eb8f31161f44fb7b71b3766
Wisdom Elue <eluewisdom@gmail.com>
2025-08-28 03:02:59 +0100

    feat: authenticate function, generate token on auth attempt

--- lib/real_deal_api/accounts.ex
+++ lib/real_deal_api/accounts.ex
@@ -8,83 +8,35 @@ defmodule RealDealApi.Accounts do
 
   alias RealDealApi.Accounts.Account
 
-  @doc """
-  Returns the list of accounts.
 
-  ## Examples
-
-      iex> list_accounts()
-      [%Account{}, ...]
-
-  """
   def list_accounts do
     Repo.all(Account)
   end
 
-  @doc """
-  Gets a single account.
 
-  Raises `Ecto.NoResultsError` if the Account does not exist.
-
-  ## Examples
-
-      iex> get_account!(123)
-      %Account{}
-
-      iex> get_account!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_account!(id), do: Repo.get!(Account, id)
 
-  @doc """
-  Creates a account.
+  def get_account_by_email(email) do
+    Account
+    |> where(email: ^email)
+    |> Repo.one()
+  end
 
-  ## Examples
 
-      iex> create_account(%{field: value})
-      {:ok, %Account{}}
-
-      iex> create_account(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_account(attrs) do
     %Account{}
     |> Account.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a account.
 
-  ## Examples
-
-      iex> update_account(account, %{field: new_value})
-      {:ok, %Account{}}
-
-      iex> update_account(account, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_account(%Account{} = account, attrs) do
     account
     |> Account.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a account.
 
-  ## Examples
-
-      iex> delete_account(account)
-      {:ok, %Account{}}
-
-      iex> delete_account(account)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_account(%Account{} = account) do
     Repo.delete(account)
   end
--- lib/real_deal_api_web/auth/guardian.ex
+++ lib/real_deal_api_web/auth/guardian.ex
@@ -18,7 +18,27 @@ defmodule RealDealApiWeb.Auth.Guardian do
       end
     end
 
-    def resorurce_from_claims(_claims) do
+    def resource_from_claims(_claims) do
       {:error, :no_id_provided}
+    end
+
+    def authenticate(email, password) do
+      case Accounts.get_account_by_email(email) do
+        nil -> {:error, :invalid_crdeentials}
+        account ->
+            case validate_password(password, account.hash_password) do
+              true -> create_token(account)
+              false -> {:error, :invalid_credentials}
+            end
+      end
+    end
+
+    defp validate_password(password, hash_password) do
+      Bcrypt.verify_pass(password, hash_password)
+    end
+
+    defp create_token(account) do
+      {:ok, token, _claims} = encode_and_sign(account)
+      {:ok, account, token}
     end
 end
