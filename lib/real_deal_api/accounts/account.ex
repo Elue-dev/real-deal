c7ca66316dedee59dff03133e71aa53d77ce32b7
Wisdom Elue <eluewisdom@gmail.com>
2025-08-28 01:35:21 +0100

    chore: setup schemas, db, entities

---
+++ lib/real_deal_api/accounts/account.ex
@@ -0,0 +1,24 @@
+defmodule RealDealApi.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :hash_password, :string
+    has_one :user, RealDealApi.Users.User
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :hash_password])
+    |> validate_required([:email, :hash_password])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "no spaces allowed. must have @ sign")
+    |> validate_length(:email, max: 160)
+    |> unique_constraint(:email)
+  end
+end
---
+++ lib/real_deal_api_web/controllers/default_controller.ex
@@ -0,0 +1,9 @@
+defmodule RealDealApiWeb.DefaultController do
+  use RealDealApiWeb, :controller
+
+  def index(conn, _params) do
+    text conn, "The Real Deal API is LIVE - #{Mix.env()}"
+  end
+end
+
+# module -> schema -> table_name
